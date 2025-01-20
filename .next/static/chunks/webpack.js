c88795ba154477095a3e7ca02467165374773abf
Mathieu Boogaerts <Booga3rtsM@gmail.com>
2025-01-20 23:26:15 +0100

    Fix: styling issue features

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("71e1bcf9aba81c78")
+/******/ 		__webpack_require__.h = () => ("ab9e5aa37769ac0c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
