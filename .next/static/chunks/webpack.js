96f61b10c450021bc14aa78022ea22a30e64fc8f
Mathieu Boogaerts <Booga3rtsM@gmail.com>
2025-01-20 18:27:29 +0100

    Fix: featured element based on viewport size

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4cea7876bb7a312b")
+/******/ 		__webpack_require__.h = () => ("ee5aa91c8ff17a24")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
@@ -6,19 +7,32 @@ import ScreenshotDemo from '../components/ScreenshotDemo';
 import Footer from '../components/Footer';
 
 export default function Home() {
+  const [isLargeViewport, setIsLargeViewport] = useState(true);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 768px)'); // Large viewport starts from 768px
+    const handleResize = () => setIsLargeViewport(mediaQuery.matches);
+
+    // Initial check
+    handleResize();
+
+    // Add listener for viewport changes
+    mediaQuery.addEventListener('change', handleResize);
+
+    // Cleanup listener on component unmount
+    return () => mediaQuery.removeEventListener('change', handleResize);
+  }, []);
+
   return (
     <>
       <Header />
       <main>
-      <Hero />
+        <Hero />
         <section id="features">
           <Features />
         </section>
         <section id="demo1">
-          <AutoCarousel />
-        </section>
-        <section id="demo3">
-          <ScreenshotDemo />
+          {isLargeViewport ? <AutoCarousel /> : <ScreenshotDemo />}
         </section>
         <section id="about">
         </section>
